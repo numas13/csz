ef5cfae25bae38759d85808d33150ddcb741f0d4
Denis Drakhnia <numas13@gmail.com>
2025-04-10 21:49:21 +0300

    use stricmp on windows

--- src/ffi.rs
+++ src/ffi.rs
@@ -13,13 +13,28 @@ mod imp {
         pub fn malloc(size: size_t) -> *mut c_void;
         pub fn free(ptr: *mut c_void);
         pub fn realloc(ptr: *mut c_void, size: size_t) -> *mut c_void;
+
+        #[cfg(unix)]
         pub fn strcasecmp(s1: *const c_char, s2: *const c_char) -> c_int;
+
+        #[cfg(windows)]
+        pub fn stricmp(s1: *const c_char, s2: *const c_char) -> c_int;
+
     }
+
+    #[cfg(windows)]
+    pub use stricmp as strcasecmp;
 }
 
 #[cfg(feature = "libc")]
 mod imp {
-    pub use libc::{free, malloc, memchr, realloc, strcasecmp, strcmp, strlen, strstr};
+    pub use libc::{free, malloc, memchr, realloc, strcmp, strlen, strstr};
+
+    #[cfg(unix)]
+    pub use libc::strcasecmp;
+
+    #[cfg(windows)]
+    pub use libc::stricmp as strcasecmp;
 }
 
 pub use imp::*;
